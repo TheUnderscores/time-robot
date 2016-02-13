6b50ff93f851c112de7220f1b5b917d02858bf97
Shelvacu <shelvacu@gmail.com>
2016-02-13 12:19:29 -0800

    impl timeline class

--- src/entity.py
+++ src/entity.py
@@ -1,11 +1,12 @@
-class EntityNotFound(BaseException)
-
+class EntityNotFound(BaseException):
+    pass
+    
 class Entity:
     def __init__(self,level=None):
         """Creates a new entity. If level is not specified, some functions (such as position) may not work"""
         self.__level = level
 
-    def position(self):
+    def position():
         """Gets the entity's current position as a Point"""
         for pos,cell in self.__level.cells:
             if self in cell:
