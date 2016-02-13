d835ee1d1892cb331cf5a57fc213165aba97bb2b
Robert Cochran <robert-git@cochranmail.com>
2016-02-13 13:25:54 -0800

    Add create_button stub in render API

--- src/render.py
+++ src/render.py
@@ -4,3 +4,11 @@ def render_board(board):
     indexed Y first, ala board[y][x]
     """
     pass
+
+def create_button(x, y, w, h, text, on_click):
+    """
+    Creates an on screen button, at (X, Y) on the screen that is
+    `w` x `h`, containing the text `text`, and provided call-back
+    in `on_click`
+    """
+    pass
