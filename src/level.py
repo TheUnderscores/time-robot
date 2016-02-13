a93d6a7b39a0f2863f595ceee3d12da325de1976
Shelvacu <shelvacu@gmail.com>
2016-02-13 10:33:21 -0800

    Level flipped, implemented and changed a few functions

--- src/level.py
+++ src/level.py
@@ -5,10 +5,10 @@ class Level:
         """Create a new level, optionally with an initial field of entities to start with. Requires either a field or dimensions"""
         if ent_field is None:
             self.ent_field = []
-            for i in range(width):
+            for i in range(height):
                 row = []
                 self.ent_field.append(row)
-                for j in range(height):
+                for j in range(width):
                     row.append([])
         else:
             self.ent_field = ent_field                    
@@ -16,23 +16,31 @@ class Level:
 
     def cells(self):
         """Iterate over each cell in the level, get a position and list of things in that cell"""
-        for i,row in enumerate(self.ent_field):
-            for j,cell in enumerate(row):
-                yield Point.new(i,j), cell
-        pass
+        for y,row in enumerate(self.ent_field):
+            for x,cell in enumerate(row):
+                yield Point.new(x,y), cell
     
     def get(self,point):
         """Returns a LIST (not a single) of entities at the given point"""
-        pass
+        return self.ent_field[self.y][self.x]
+
+    def empty(self,point):
+        """Returns true if there is nothing at point"""
+        return not self.get(point)
 
     def add(self,ent,point):
         """Adds an entity to the field at position point"""
-        pass
+        self.ent_field[point.y][point.x]
 
-    def move(self,from_point, to_point):
-        """Moves an entity from one position to another"""
-        pass
+    def move(self, ent, to_point):
+        """Moves an entity from wherever it is to to_point"""
+        for point, cell in self.cells():
+            for i, ent in enumerate(cell):
+                if ent in cell:
+                    self.add(cell.delete(ent),to_point)
+                    return
+                
 
     def destroy(self,point):
-        """Destroys an entity at point"""
-        pass
+        """Destroys all entities at point"""
+        self.ent_field[self.y][self.x] = []
