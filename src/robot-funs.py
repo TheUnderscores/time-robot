c71be6405071bb4906efca68f9623cb2ddb0623d
Niko Geil <code@serversquared.org>
2016-02-13 20:15:15 -0800

    Add robot functions stub.

---
+++ src/robot-funs.py
@@ -0,0 +1,17 @@
+def move_up(robot):
+	pass
+
+def move_down(robot):
+	pass
+
+def move_left(robot):
+	pass
+
+def move_right(robot):
+	pass
+
+def reverse_time(ticks):
+	pass
+
+def set_time(tick):
+	pass
