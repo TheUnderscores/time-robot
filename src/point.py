678a17656504f46a1b5b13edf115d248ffc93330
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2016-02-14 00:29:47 -0800

    Add overload methods to Point

--- src/point.py
+++ src/point.py
@@ -6,3 +6,22 @@ class Point:
     def add(self, a, b):
         """ Returns a new point containing the addition of the coordinates of a & b """
         return Point(a.x+b.x,a.y+b.y)
+
+    def __sub__(self, i):
+        return Point(self.x-i.x, self.y-i.y)
+
+    def __mul__(self, i):
+        if isinstance(i, Point):
+            return Point(self.x*i.x, self.y*i.y)
+        else:
+            return Point(self.x*i, self.y*i)
+
+    def __truediv__(self, i):
+        if isinstance(i, Point):
+            return Point(self.x/i.x, self.y/i.y)
+        else:
+            return Point(self.x/i, self.y/i)
+
+    def castInt(self):
+        self.x = int(self.x)
+        self.y = int(self.y)
